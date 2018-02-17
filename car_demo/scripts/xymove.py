6321e1ad7fdd66d28ef38ef799a9abb16b3d0ac5
cretaceous-creature <xychen@jsk.imi.i.u-tokyo.ac.jp>
2018-02-17 19:37:17 +0900

    add xy mover, first control the xy plane coordinate, then control the arm, results in less unsupposed trajectory

--- car_demo/scripts/xymove.py
+++ car_demo/scripts/xymove.py
@@ -13,38 +13,30 @@ rospy.init_node('move_group_python_interface_tutorial',
 
 robot = moveit_commander.RobotCommander()
 scene = moveit_commander.PlanningSceneInterface()
-group = moveit_commander.MoveGroupCommander("manipulator")
+group = moveit_commander.MoveGroupCommander("xyeffector")
 display_trajectory_publisher = rospy.Publisher(
                                     '/ur5/move_group/display_planned_path',
                                     moveit_msgs.msg.DisplayTrajectory,
                                     queue_size=20)
 
-#group.set_pose_reference_frame("base_link")
-#group.set_end_effector_link("ee_link")
 print "============ Reference frame: %s" % group.get_planning_frame()
 print "============ End effector: %s" % group.get_end_effector_link()
 print "============ Robot Groups:"
 print robot.get_group_names()
 print robot.get_current_state()
-group.set_planning_time(10.0)
 print "default plan time %f" % group.get_planning_time()
 
-group.clear_pose_targets()
-
 group.set_goal_position_tolerance(0.0005)
-pose_target = geometry_msgs.msg.Pose()
-quat = [ -0.3043807, -0.5272029, 0.3966767, 0.6870641 ]
-pose_target.orientation.x = quat[0]
-pose_target.orientation.y = quat[1]
-pose_target.orientation.z = quat[2]
-pose_target.orientation.w = quat[3]
-pose_target.position.x = 1.19
-pose_target.position.y = 3.56
-pose_target.position.z = 0.62
-group.set_pose_target(pose_target)
+group.set_goal_joint_tolerance(0.0005)
 
+joints_value = group.get_joint_value_target()
 
-group.set_planner_id("PRMkConfigDefault")
+joints_value[0] = 3.3
+joints_value[1] = 0.9
+
+group.set_joint_value_target(joints_value)
+
+#group.set_planner_id("PRMkConfigDefault")
 
 #plan1 = group.plan()
 
